41edf19f6f9b23e9cc1b735c2fb8c93b4cea6888
Felipe <lipesmaffini@gmail.com>
2023-11-24 00:06:59 -0400

    Add project files.

---
+++ Udemy_LINQ/Program.cs
@@ -0,0 +1,57 @@
+﻿using System.Globalization;
+using Udemy_LINQ.Entities;
+
+static void Print<T>(string message, IEnumerable<T> collection)
+{
+    Console.WriteLine(message);
+    foreach(T obj in collection)
+    {
+        Console.WriteLine(obj);
+    }
+    Console.WriteLine();
+
+}
+
+
+
+
+Console.Write("Enter file name(with extension): ");
+string fileName = Console.ReadLine();
+
+
+var info = new DirectoryInfo(Environment.CurrentDirectory);
+var path = info.Parent.Parent.Parent.Parent.FullName + Path.DirectorySeparatorChar;
+
+
+try
+{
+    Console.Write("Enter salary: ");
+    double value = double.Parse(Console.ReadLine());
+    string[] line;
+    List<Employee> employees = new List<Employee>();
+    using (StreamReader sr = File.OpenText(path + fileName))
+    {
+        while (!sr.EndOfStream)
+        {
+            line = sr.ReadLine().Split(';');
+            employees.Add(new Employee { Name = line[0], Email = line[1], Salary = double.Parse(line[2],CultureInfo.InvariantCulture) });
+           
+        }
+        Console.WriteLine();
+        var biggerThanValue =
+             from employee in employees
+             where employee.Salary > value
+             orderby employee.Name
+             select employee.Email;
+        Print($"Email of people whose salary is more than {value.ToString("F2", CultureInfo.InvariantCulture)}: ",
+            biggerThanValue);
+
+
+
+    }
+
+}
+catch (IOException e)
+{
+    Console.WriteLine(e.Message);
+}
